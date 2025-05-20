e2aad3595ffb88dde7217f76c4074d06f409baee
MohammadAsad-Weber <mohdasad69690@gmail.com>
2025-05-20 20:14:57 +0530

    Fallback UI (Loader)

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="w-full h-dvh flex items-center justify-center">
+      <span className="loader"></span>;
+    </div>
+  );
+}
+
+export default Loading;
